1890365e462d9f7f5556f0a8cbc9a356fe24062d
Oleg Markoff <omarkoff@1800flowers.com>
2023-03-31 11:32:16 -0400

    fix: PWA-000 update on chart

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -3,6 +3,35 @@ import { Chart } from "react-google-charts";
 import response  from "./data.json";
 
 const Charts = () => {
+  // const chartOptions = {
+  //   title: 'LCP distribution',
+  //   legend: { position: 'top', maxLines: 2 },
+  //   colors: ["#7dd87d", "#ff6f3c", "#dc2f2f"],
+  //   interpolateNulls: false,
+  // }
+  const chartOptions = {
+    title : 'CrUX report on HD',
+    vAxis: {title: '%'},
+    hAxis: {title: 'Metrics name'},
+    seriesType: 'bars',
+    legend: { position: 'top', maxLines: 2 },
+    colors: ["#7dd87d", "#ff6f3c", "#dc2f2f"],
+    series: {5: {type: 'line'}}
+  };
+  const chartData= [
+    [
+      'Metric',
+      'good',
+      'mid',
+      'bad'
+    ],
+    ['largest_contentful_pain',  43.26, 28.07, 28.66],
+    ['cumulative_layout_shift',  28.93, 36.91, 35.16],
+    ['experimental_interaction_to_next_paint',  47.82, 37.57, 14.60],
+    ['experimental_time_to_first_byte', 64.81, 22.81, 12.38],
+    ['first_contentful_pain',  88.97, 5.72, 5.31],
+    ['first_input_delay',40.61, 18.86, 40.53]
+  ];
   const[chart, setChart] = useState(response);
   // useEffect(() => {
   //   setChart(response);
@@ -11,9 +40,9 @@ const Charts = () => {
   console.log("chartType", chart.charts[0].name)
   return (
     <Chart
-      chartType={chart?.charts[0]?.chartType}
-      data={chart?.charts[0]?.data}
-      options={chart?.charts[0]?.options}
+      data={chartData}
+      options={chartOptions}
+      chartType="ComboChart"
       width="100%"
       height="400px"
     />
